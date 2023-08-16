9a41992cb2178b352ba882ea16dc9eaf4aba40f4
Will <william@wnatechlabs.com>
2023-08-16 02:24:56 -0400

    add base home page

--- lib/ui/pages/home/page.dart
+++ lib/ui/pages/home/page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:phononxgit/core/core.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({super.key = const Key('HomePage')});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppConstants.appName),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(),
+            Gap(8),
+            Expanded(
+              child: Placeholder(
+                fallbackHeight: 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
