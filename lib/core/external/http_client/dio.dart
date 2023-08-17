707735d9e68abbe3ed661e6b7e578d1e637f9c6d
Will <william@wnatechlabs.com>
2023-08-17 18:35:49 -0400

    setup http client

--- lib/core/external/http_client/dio.dart
+++ lib/core/external/http_client/dio.dart
@@ -1,7 +1,13 @@
 import 'package:dio/dio.dart';
 
-Dio httpClient = Dio(BaseOptions(
-  baseUrl: 'myhost',
-  connectTimeout: const Duration(seconds: 2),
-  sendTimeout: const Duration(seconds: 12),
-));
+Dio httpClient = Dio(
+  BaseOptions(
+    baseUrl: 'https://api.github.com',
+    connectTimeout: const Duration(seconds: 12),
+    sendTimeout: const Duration(seconds: 12),
+    headers: {
+      "Authorization":
+          "Bearer <your personal github's fina grained token here>",
+    },
+  ),
+);
